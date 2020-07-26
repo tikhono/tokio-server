469bb95185b50e9c71b6bdd6113c72878cf8effc
tikhono <tihonovandrey11@gmail.com>
2020-07-27 02:36:07 +0300

    set first sended number as u8

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
     let addr = "127.0.0.1:6142";
 
     let mut listener = TcpListener::bind(addr).await.unwrap();
-    //    uncomment to see how server works asyncronosly with 2 or more connected clients
+    //    uncomment to explisitly see how server works asyncronosly with 2 or more clients
     //    thread::sleep(time::Duration::from_millis(
     //        rand::thread_rng().gen_range(3000, 6000),
     //    ));
@@ -22,13 +22,15 @@ async fn main() {
                     Ok(mut sock) => {
                         tokio::spawn(async move {
                             let (_reader, mut writer) = sock.split();
-                            let cap: i64 = rand::thread_rng().gen_range(1, 21);
+                            //let cap: i64 = rand::thread_rng().gen_range(1, 21);
+                            let cap: u8 = rand::thread_rng().gen_range(1, 21);
                             let numbers: Vec<i64> = (0..cap)
                                 .map(|_| rand::thread_rng().gen_range(1, 21))
                                 .collect();
                             println!("cap is: {}", cap);
                             println!("numbers is: {:?}", numbers);
-                            match writer.write_i64(cap).await {
+                            //match writer.write_i64(cap).await {
+                            match writer.write_u8(cap).await {
                                 Ok(_amt) => {
                                     println!("wrote {}", cap);
                                 }
